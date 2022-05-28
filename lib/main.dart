6090cdd9f95829df5f821d19e6fa749d22b3dc58
ToumariHikana <toumarihikana@gmail.com>
2022-05-28 20:59:50 +0900

    カウントの表示ウィジェットを別ファイルに分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_test_for_smartphone/providers/count_provider.dart';
+import 'package:flutter_web_test_for_smartphone/widgets/main_contets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'widgets/bottom_app_bar.dart';
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -78,42 +79,13 @@ class MyHomePage extends HookConsumerWidget {
         // the App.build method, and use it to set our appbar title.
         title: Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              ref.watch(countProvider).toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: const MainContents(),
       floatingActionButton: FloatingActionButton(
         onPressed: (() => _incrementCounter(ref)),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndDocked,
       bottomNavigationBar: BottomBar(
         stateCheckStr: ref.watch(countProvider).toString(),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ lib/widgets/main_contets.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../providers/count_provider.dart';
+
+class MainContents extends HookConsumerWidget {
+  const MainContents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // build メソッド内でフックを使用できます。
+    final state = useState(0);
+
+    // `ref` オブジェクトを使ってプロバイダを監視することもできます。
+    final counter = ref.watch(countProvider);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          const Text(
+            '別ファイル:',
+          ),
+          Text(
+            counter.toString(),
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+    );
+  }
+}
